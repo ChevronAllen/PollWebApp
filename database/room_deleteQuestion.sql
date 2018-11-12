4f04ce812ee67d096bad708dd2f719d459a4253f
Chevron Allen <chevronallen@gmail.com>
2018-11-11 22:42:20 -0500

    Documentation Update, added function documentation
    added Error definitions,
    added Logout
    parameter changed addQuestion
    Added Room Get Question
    Added Room Deletion
    find poll returns a new column

---
+++ database/room_deleteQuestion.sql
@@ -0,0 +1,18 @@
+CREATE DEFINER=`ermine`@`%` PROCEDURE `room_deleteQuestion`(
+	IN uID VARCHAR(255),
+    IN uSession VARCHAR(255),
+    IN qID VARCHAR(255),
+    OUT err INT
+)
+BEGIN
+	SET err = 1;
+	SET @valid = fn_isValidSession(uID,uSession);
+    
+    IF @valid THEN 
+		DELETE FROM Questions
+        WHERE questionID = qID AND userID = uID;
+        SET err = 0;
+	ELSE 
+		SET err = 2;
+    END IF;
+END
