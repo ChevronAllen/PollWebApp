e25784c6695fbc86ad6e986bdbb8d0771b3797da
Pheasant-King <waskew122@yahoo.com>
2018-11-29 12:19:47 -0500

    Update TestAnswerQuestion.php

--- API/Test/TestAnswerQuestion.php
+++ API/Test/TestAnswerQuestion.php
@@ -1,1 +1,32 @@
+<?php
 
+  returnWithInfo("8");
+
+  function returnWithInfo($correctChoice_)
+  {
+	  $retValue = createJSONString($correctChoice_, "");
+	  sendResultInfoAsJson( $retValue );
+  }
+
+  function createJSONString($correctChoice_, $error_)
+  {
+	$ret = '
+        {
+          "correctChoice" : '. $correctChoice_ .' ,
+          "error" : "' . $error_ . '"
+        }';
+	return $ret;
+  }
+  
+  function sendResultInfoAsJson( $obj )
+  {  
+	header('Content-type: application/json');
+	echo $obj;
+  }
+  
+  function returnWithError( $err , $sqlerr)
+  {
+	$retValue = createJSONString("",$err,$sqlerr);
+	sendResultInfoAsJson( $retValue );
+  }
+?>
