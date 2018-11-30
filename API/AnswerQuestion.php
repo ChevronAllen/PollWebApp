c92b9901779413d59858d1ad3b7e3ebb47612751
Pheasant-King <waskew122@yahoo.com>
2018-11-30 00:11:07 -0500

    Updated AnswerQuestion.php

--- API/AnswerQuestion.php
+++ API/AnswerQuestion.php
@@ -1,21 +1,21 @@
 <?php
 	require("config.php");
-	
+
 	//	connection using the sql credentials
 	$connection = new mysqli($serverIP, $serverUSER, $serverPASS, $serverDB, $serverPORT)
 	or die('connection to server failed');
 	//	Get JSON input
 	$inData = json_decode(file_get_contents('php://input'), true);
-	
+
 	$userID = "";
 	$sessionID = "";
 	$roomID = "";
 	$questionID = "";
 	$choice = -1;
-	
+
 	if($connection->connect_error)
 	{
-		returnWithError("Error Connecting to the Server");
+		returnWithError(5, "Error Connecting to the Server");
 	}
 	else
 	{
@@ -24,7 +24,7 @@
 		$roomID = mysqli_real_escape_string($connection, $inData["roomID"]);
 		$questionID = mysqli_real_escape_string($connection, $inData["questionID"]);
 		$choice = mysqli_real_escape_string($connection, $inData["choice"]);
-		
+
 		$call = 'CALL PollingZone.????(
 			   "' . $userID . '",
   			   "' . $sessionID . '",
@@ -32,53 +32,54 @@
   			   "' . $questionID . '",
   			   "' . $choice . '"
                 );';
-				
+
 		$result = $connection->query($call);
 		if($result->num_rows == NULL || $result->num_rows == 0)
 		{
 			$sqlReport = $mysqli_error;
 			if($result->num_rows == NULL)
-				returnWithError("Invalid User Credentials.", $sqlReport);
-			
-			returnWithError("Failed to answer question.", $sqlReport);
+				returnWithError(1, "Invalid User Credentials.";
+
+			returnWithError(2, "Failed to answer question.");
 		}
 		else
 		{
 			$row = $result->fetch_assoc();
 			$correctChoice = $row["correctChoice"];
-			
+
 			returnWithInfo($correctChoice);
 		}
-		
+
 	}
 	// Close the connection
 	$connection->close();
-	
+
   function returnWithInfo($correctChoice_)
   {
-	  $retValue = createJSONString($correctChoice_, "");
+	  $retValue = createJSONString($correctChoice_, "", 0);
 	  sendResultInfoAsJson( $retValue );
   }
 
-  function createJSONString($correctChoice_, $error_)
+  function createJSONString($correctChoice_, $error_, $errCode)
   {
 	$ret = '
         {
           "correctChoice" : '. $correctChoice_ .' ,
-          "error" : "' . $error_ . '"
+          "error" : "' . $error_ . '",
+	  "errorCode" : "' . $errCode . '"
         }';
 	return $ret;
   }
-  
+
   function sendResultInfoAsJson( $obj )
-  {  
+  {
 	header('Content-type: application/json');
 	echo $obj;
   }
-  
-  function returnWithError( $err )
+
+  function returnWithError($errCode, $err)
   {
-	$retValue = createJSONString("",$err);
+	$retValue = createJSONString("",$err, $errCode);
 	sendResultInfoAsJson( $retValue );
   }
 ?>
