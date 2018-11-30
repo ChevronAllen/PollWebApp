7a888f1a9b8b2cfa9b0ab1d5ec29a1223b043c0d
Chevron Allen <chevronallen@yahoo.com>
2018-11-30 15:04:56 -0500

    roomGetAnalytics

--- database/room_getAnalytics.sql
+++ database/room_getAnalytics.sql
@@ -17,6 +17,30 @@ BEGIN
 											percentCorrect DOUBLE
 									);
     
-
+    INSERT INTO UserGrades (userID, orgID,numCorrect,numWrong)
+    SELECT U.userID,U.user_optionalName,(
+				SELECT SUM(R2.selection = Q2.correctResponse) 
+                FROM Responses AS R2 INNER JOIN  Questions AS Q2
+                WHERE userID = U.userID AND Q2.correctResponse != 0
+            ), (
+				SELECT SUM(R2.selection != Q2.correctResponse) 
+                FROM Responses AS R2 INNER JOIN  Questions AS Q2
+                WHERE userID = U.userID AND Q2.correctResponse != 0
+			)
+            FROM Users AS U RIGHT JOIN Responses AS R
+				ON U.userID = R.userID
+			WHERE (userID IS NOT NULL) AND roomID = rID;
+	
+    INSERT INTO UserGrades (questionID ,percentCorrect)
+    SELECT questionID, (
+		SELECT SUM(R2.selection = Q2.correctResponse)/COUNT(Q2.questionID)
+        FROM Questions AS Q2 RIGHT JOIN Responses AS R2
+			ON Q2.questionID = R2.questionID
+		WHERE Q2.questionID = Q.questionID
+    ) FROM Questions AS Q
+    WHERE correctResponse !=0;
+	
+    SELECT * FROM UserGrades;
+    SELECT * FROM QuestionGrades;
 	DROP TEMPORARY TABLE UserGrades, QuestionGrades;
 END
