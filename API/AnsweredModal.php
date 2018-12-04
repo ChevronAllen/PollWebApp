e0ad217fd7728dbee3f903b4a23afb9a63904c72
Pheasant-King <waskew122@yahoo.com>
2018-12-04 17:57:28 -0500

    Fixed error handling AnsweredModal.php

--- API/AnsweredModal.php
+++ API/AnsweredModal.php
@@ -1,19 +1,17 @@
 <?php
 	require("config.php");
-
 	//	connection using the sql credentials
 	$connection = new mysqli($serverIP, $serverUSER, $serverPASS, $serverDB, $serverPORT)
 	or die('connection to server failed');
 	//	Get JSON input
 	$inData = json_decode(file_get_contents('php://input'), true);
-
 	$userID = "";
 	$sessionID = "";
 	$roomID = "";
-	
-    class QuestionResult 
+
+    class QuestionResult
 	{
-      function __construct() 
+      function __construct()
 	  {
 		$this->questionID = "";
 		$this->userResponse = "";
@@ -21,7 +19,6 @@
 		$this->correct = "";
       }
 	}
-
 	if($connection->connect_error)
 	{
 		returnWithError(1, "Error Connecting to the Server");
@@ -31,22 +28,22 @@
 		$userID = mysqli_real_escape_string($connection, $inData["userID"]);
 		$sessionKey = mysqli_real_escape_string($connection, $inData["sessionID"]);
 		$roomID = mysqli_real_escape_string($connection, $inData["roomID"]);
-
 		$call = 'CALL PollingZone.user_getAnalytics(
 			   "' . $userID . '",
   			   "' . $sessionID . '",
   			   "' . $roomID . '"
                 );';
-
 		$result = $connection->query($call);
-
-		if($result->num_rows == NULL || $result->num_rows == 0)
+		if($result->num_rows == NULL)
 		{
-			if($result->num_rows == NULL)
 				returnWithError(2, "Invalid User Credentials.");
-
-			returnWithError(3, "Failed to access stats or no stats for this poll.");
+        exit();
 		}
+    else if($result->num_rows == 0)
+    {
+      returnWithError(3, "Failed to access stats or no stats for this poll.");
+      exit();
+    }
 		else
 		{
 			$roomResult = array();
@@ -57,28 +54,28 @@
 				$question->userResponse = $row["userResponse"];
 				$question->correctResponse = $row["correctResponse"];
 				$question->correct = $row["correct"];
-				
+
 				$roomResult[] = $question;
 			}
-			
+
 			sendWithInfo(json_encode($roomResult));
 		}
 	}
 	// Close the connection
 	$connection->close();
-	
+
   function returnWithError($errCode, $err )
   {
     $retValue = createJSONString("",$err, $errCode);
     sendResultInfoAsJson( $retValue );
   }
-  
+
   function returnWithInfo($roomResult_)
   {
 	  $retValue = createJSONString($roomResult_, "", 0);
 	  sendResultInfoAsJson( $retValue );
   }
-  
+
   function createJSONString($roomResult_, $error_, $errCode_)
   {
 		$ret = '
@@ -89,7 +86,7 @@
         }';
     return $ret;
   }
-  
+
   function sendResultInfoAsJson( $obj )
   {
     header('Content-type: application/json');
