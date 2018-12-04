b2411fdeb071230dc3be90731d06946fa10f239d
Pheasant-King <waskew122@yahoo.com>
2018-12-04 17:47:04 -0500

    fixed error handling

--- API/AdminPage.php
+++ API/AdminPage.php
@@ -1,31 +1,24 @@
 <?php
-/*
-admin gets
-4 - created 
-4 - answered
-all ongoing and upcoming polls
-*/
-    class Room 
+    class Room
 	{
-      function __construct() 
+      function __construct()
 	  {
 		$this->roomID = "";
 		$this->roomCode = "";
       }
 	}
-
 	require("config.php");
-	
+
 	//	connection using the sql credentials
 	$connection = new mysqli($serverIP, $serverUSER, $serverPASS, $serverDB, $serverPORT)
 	or die('connection to server failed');
 	//	Get JSON input
 	$inData = json_decode(file_get_contents('php://input'), true);
-	
+
 	$userID = "";
 	$sessionID = "";
 	$today = getDate();
-	
+
 	if($connection->connect_error)
 	{
 		returnWithError(1, "Error Connecting to the Server");
@@ -34,27 +27,29 @@ all ongoing and upcoming polls
 	{
 		$userID = mysqli_real_escape_string($connection, $inData["userID"]);
 		$sessionID = mysqli_real_escape_string($connection, $inData["sessionID"]);
-		
+
 		$call = 'CALL PollingZone.user_getRoomsOwned(
 			   "' . $userID . '",
   			   "' . $sessionID . '"
                 );';
-		
+
 		$result = $connection->query($call);
-		
+
 		if ($result == NULL || $result->num_rows == 0)
 		{
-			if($result == NULL)
 				returnWithError(2, "User authentication error. Please login");
-			
-			//returnWithError(0, "Parameter error/No rooms owned by this user");
-			$CreatedRooms = "No rooms owned by this user";
+        exit();
 		}
+    else if($result->num_rows == 0)
+    {
+      $CreatedRooms = "No rooms owned by this user";
+
+    }
 		else
 		{
 		  $CreatedRooms = array();
 		  $num_rows = mysql_num_rows($result);
-		  
+
 		  while($row = $result->fetch_assoc())
 		  {
 			if($num_rows < 4)
@@ -64,7 +59,7 @@ all ongoing and upcoming polls
 					$roomC = new Room();
 					$roomC->roomID = $row["roomID"];
 					$roomC->roomCode = $row["roomCode"];
-					
+
 					$CreatedRooms[] = $roomC;
 				}
 			}
@@ -75,7 +70,7 @@ all ongoing and upcoming polls
 					$roomC = new Room();
 					$roomC->roomID = $row["roomID"];
 					$roomC->roomCode = $row["roomCode"];
-					
+
 					$CreatedRooms[] = $roomC;
 				}
 			}
@@ -83,22 +78,24 @@ all ongoing and upcoming polls
 		  }
 		}
 		$connection->next_result();
-		
+
 		$call = 'CALL PollingZone.user_getSubmissions(
 			   "' . $userID . '",
-  			   "' . $sessionID . '"
+  			 "' . $sessionID . '"
                 );';
-		
-		
+
+
 		$result = $connection->query($call);
-		if ($result == NULL || $result->num_rows == 0)
+		if ($result == NULL)
 		{
-			if($result == NULL)
 				returnWithError(2, "User authentication error. Please login");
-			
-			//returnWithError(0, "Parameter error/No rooms answered by this user");
-			$AnsweredRooms = "No rooms answered by this user";
+			  exit();
 		}
+    else if($result->num_rows == 0)
+    {
+      $AnsweredRooms = "No rooms answered by this user";
+
+    }
 		else
 		{
 		  $AnsweredRooms = array();
@@ -112,7 +109,7 @@ all ongoing and upcoming polls
 					$roomA = new Room();
 					$roomA->roomID = $row["roomID"];
 					$roomA->roomCode = $row["roomCode"];
-					
+
 					$AnsweredRooms[] = $roomA;
 				}
 			}
@@ -123,7 +120,7 @@ all ongoing and upcoming polls
 					$roomA = new Room();
 					$roomA->roomID = $row["roomID"];
 					$roomA->roomCode = $row["roomCode"];
-					
+
 					$AnsweredRooms[] = $roomA;
 				}
 			}
@@ -131,23 +128,25 @@ all ongoing and upcoming polls
 		  }
 		}
 		$connection->next_result();
-		
+
 		$call = 'CALL PollingZone.user_getRoomsOwned(
 			   "' . $userID . '",
-  			   "' . $sessionID . '"
+  			 "' . $sessionID . '"
                 );';
-		
+
 		$result = $connection->query($call);
-		
-		if ($result == NULL || $result->num_rows == 0)
+
+		if ($result == NULL)
 		{
-			if($result == NULL)
 				returnWithError(2, "User authentication error. Please login");
-			
-			returnWithError(1, "Parameter error/No rooms owned by this user");
+        exit();
 		}
+    else if($result->num_rows == 0)
+    {
+      returnWithError(1, "Parameter error/No rooms owned by this user");
+    }
 		else
-		{		
+		{
 		  while($row = $result->fetch_assoc())
 		  {
 			$roomExpirationDate = date_parse($row["dateExpire"]);
@@ -162,27 +161,27 @@ all ongoing and upcoming polls
 				$roomO = new Room();
 				$roomO->roomID = $row["roomID"];
 				$roomO->roomCode = $row["roomCode"];
-				
+
 				$RemainingRooms[] = $roomO;
 			   }
 		  }
 		}
-		
+
 		returnWithInfo(json_encode($CreatedRooms), json_encode($AnsweredRooms), json_encode($RemainingRooms));
 	}
-	
+
   function returnWithError($errCode, $err )
   {
     $retValue = createJSONString("","","",$err, $errCode);
     sendResultInfoAsJson( $retValue );
   }
-  
+
   function returnWithInfo($CreatedRooms, $AnsweredRooms, $RemainingRooms)
   {
 	  $retValue = createJSONString($CreatedRooms, $AnsweredRooms, $RemainingRooms, "", 0);
 	  sendResultInfoAsJson( $retValue );
   }
-  
+
   function createJSONString($CreatedRooms_, $AnsweredRooms_, $RemainingRooms_, $error_, $errCode_)
   {
 		$ret = '
@@ -190,12 +189,12 @@ all ongoing and upcoming polls
 		  "createdRooms" : "' . $CreatedRooms_ . '",
 		  "answeredRooms" : "' . $AnsweredRooms_ . '",
 		  "remainingRooms" : "' . $RemainingRooms_ . '",
-          "error" : "' . $error_ . '",
+      "error" : "' . $error_ . '",
 		  "errorCode" : "' . $errCode_ . '"
         }';
     return $ret;
   }
-  
+
   function sendResultInfoAsJson( $obj )
   {
     header('Content-type: application/json');
